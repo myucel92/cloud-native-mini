a9c8e3ad71dbb5e59113b4c2509080e95df691fb
myucel <myucel2438@gmail.com>
2026-05-20 14:01:30 +0300

    Add ingress and monitoring metrics

--- app/app.py
+++ app/app.py
@@ -1,58 +1,30 @@
-from flask import Flask, jsonify, Response, request
-from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
-import time
+from flask import Flask, jsonify, Response
+from prometheus_client import Counter, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
 REQUEST_COUNT = Counter(
-    "flask_app_requests_total",
-    "Total number of requests",
-    ["method", "endpoint", "http_status"]
+    "app_requests_total",
+    "Total App HTTP Request Count"
 )
-
-REQUEST_LATENCY = Histogram(
-    "flask_app_request_latency_seconds",
-    "Request latency in seconds",
-    ["endpoint"]
-)
-
-@app.before_request
-def before_request():
-    request.start_time = time.time()
-
-@app.after_request
-def after_request(response):
-    request_latency = time.time() - request.start_time
-
-    REQUEST_COUNT.labels(
-        method=request.method,
-        endpoint=request.path,
-        http_status=response.status_code
-    ).inc()
-
-    REQUEST_LATENCY.labels(
-        endpoint=request.path
-    ).observe(request_latency)
-
-    return response
 
 @app.route("/")
 def home():
+    REQUEST_COUNT.inc()
+
     return jsonify({
         "message": "Cloud Native Mini Project is running!",
         "status": "success",
         "version": "v1"
     })
 
-@app.route("/health")
-def health():
-    return jsonify({
-        "status": "healthy"
-    })
-
 @app.route("/metrics")
 def metrics():
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+    return Response(
+        generate_latest(),
+        mimetype=CONTENT_TYPE_LATEST
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
